696d6beabce5618de95c699074f7479c9302fa2a
JJunJia <jjjunjia97.23@ichat.sp.edu.sg>
2024-06-18 17:00:10 +0800

    Did qn 2d

---
+++ led_exercise_2d.py
@@ -0,0 +1,21 @@
+import hal.hal_led as led
+import time
+from time import sleep
+import hal.hal_input_switch as switch
+
+led.init()
+switch.init()
+while(True):
+    if switch.read_slide_switch()==1:
+            led.set_output(24,5)
+            time.sleep(0.2)
+            led.set_output(24,0)
+            time.sleep(0.2)
+    elif switch.read_slide_switch()==0:
+        stop_time = time.time() + 5  
+        while time.time() <= stop_time:
+          led.set_output(24,5)
+          time.sleep(0.1)
+          led.set_output(24,0)
+          time.sleep(0.1)
+        break
